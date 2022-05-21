c01097c07382f23963bae9a1dbe6132f5a426181
Jaeuk Kim <ksyj2006@naver.com>
2022-05-21 14:30:05 +0900

    0521
    0521

---
+++ BOJ/Silver/2193.java
@@ -0,0 +1,25 @@
+package p2193_이친수_Silver3;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) throws Exception{
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int n = Integer.parseInt(br.readLine());
+		long[] dp = new long[91];
+		
+		dp[0] = 0;
+		dp[1] = 1;
+		dp[2] = 1;
+		
+		for(int i=3; i<=90; i++) {
+			dp[i] = dp[i-1] + dp[i-2];
+		}
+		
+		System.out.println(dp[n]);	
+		
+	}
+}
