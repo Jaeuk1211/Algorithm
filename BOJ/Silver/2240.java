be7658f9d7d023e8a0b067df2ac1ac3338ffaf01
Jaeuk Kim <ksyj2006@naver.com>
2022-05-23 10:45:38 +0900

    0523
    0523

---
+++ BOJ/Silver/2240.java
@@ -0,0 +1,15 @@
+package p2240_자두나무_Silver1;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) throws Exception {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		
+		
+	}
+	
+}
